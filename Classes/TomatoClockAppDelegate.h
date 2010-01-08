303154d03f7e4d7b11995d77218bf50b0c527cfc
GangeyaKumar <gk@gangeyakumars-macintosh.local>
2010-01-08 09:45:12 +0600

    Changed Media Folder

--- Classes/TomatoClockAppDelegate.h
+++ Classes/TomatoClockAppDelegate.h
@@ -7,16 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+@class Introduction;
 @class Tomatoview;
-
 @interface TomatoClockAppDelegate : NSObject <UIApplicationDelegate> {
     UIWindow *window;
+	Introduction *introduction;
 	Tomatoview *tomatoview;
     
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-@property (nonatomic, retain)  Tomatoview *tomatoview;
-
+@property (nonatomic, retain) IBOutlet Introduction *introduction;
+@property (nonatomic, retain) Tomatoview *tomatoview;
 @end
+
 
